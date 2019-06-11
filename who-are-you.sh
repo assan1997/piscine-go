d3d58ec53b15072b7912ee2c0b7e04144d969b50
assan1997 <nickassan01@gmail.com>
2019-06-11 13:31:26 +0000

    retrying

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json?id=70 | jq ' .[] | .name'
+curl  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ' .[] | select( .id == 70 ).name'
