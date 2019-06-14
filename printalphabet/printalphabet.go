4425f91f5314ad3f56e9c958945902f359ee4ea5
assan1997 <nickassan01@gmail.com>
2019-06-14 17:58:15 +0000

    My golang

--- printalphabet/printalphabet.go
+++ printalphabet/printalphabet.go
@@ -1,8 +1,62 @@
 package main 
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func main(){
 
-	fmt.Println("abcdefghijklmnopqrstwxyz")
+	variableA :='a'
+	variableB :='b'
+	variableC :='c'
+	variableD :='d'
+	variableE :='e'
+	variableF :='f'
+	variableG :='g'
+	variableH :='h'
+	variableI :='i'
+	variableJ :='j'
+	variableK :='k'
+	variableL :='l'
+	variableM :='m'
+	variableN :='n'
+	variableO :='o'
+	variableP :='p'
+	variableQ :='q'
+	variableR :='r'
+	variableS :='s'
+	variableT :='t'
+	variableU :='u'
+	variableV :='v'
+	variableW :='w'
+	variableX :='x'
+	variableY :='y'
+	variableZ :='z'
+
+	z01.PrintRune(variableA)
+	z01.PrintRune(variableB)
+	z01.PrintRune(variableC)
+	z01.PrintRune(variableD)
+	z01.PrintRune(variableE)
+	z01.PrintRune(variableF)
+	z01.PrintRune(variableG)
+	z01.PrintRune(variableH)
+	z01.PrintRune(variableI)
+	z01.PrintRune(variableJ)
+	z01.PrintRune(variableK)
+	z01.PrintRune(variableL)
+	z01.PrintRune(variableM)
+	z01.PrintRune(variableN)
+	z01.PrintRune(variableO)
+	z01.PrintRune(variableP)
+	z01.PrintRune(variableQ)
+	z01.PrintRune(variableR)
+	z01.PrintRune(variableS)
+	z01.PrintRune(variableU)
+	z01.PrintRune(variableV)
+	z01.PrintRune(variableW)
+	z01.PrintRune(variableX)
+	z01.PrintRune(variableY)
+	z01.PrintRune(variableZ)
+
+	z01.PrintRune('\n')
+	
 }
