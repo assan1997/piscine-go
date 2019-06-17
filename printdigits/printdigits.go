4657a4d2cc0d07c5661816775a67cdf220510f7c
assan1997 <nickassan01@gmail.com>
2019-06-17 18:40:01 +0000

    my m

--- printdigits/printdigits.go
+++ printdigits/printdigits.go
@@ -1,11 +1,31 @@
 package main 
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func main(){
 
-fmt.Println("0123456789")
+	variableA :='0'
+	variableB :='1'
+	variableC :='2'
+	variableD :='3'
+	variableE :='4'
+	variableF :='5'
+	variableG :='6'
+	variableH :='7'
+	variableI :='8'
+	variableJ :='9'
 
-fmt.Println('\n')
+	z01.PrintRune(variableA)
+	z01.PrintRune(variableB)
+	z01.PrintRune(variableC)
+	z01.PrintRune(variableD)
+	z01.PrintRune(variableE)
+	z01.PrintRune(variableF)
+	z01.PrintRune(variableG)
+	z01.PrintRune(variableH)
+	z01.PrintRune(variableI)
+	z01.PrintRune(variableJ)
+	
+	z01.PrintRune('\n')
 
 }
